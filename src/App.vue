8ca3b65996ea58d09bc1f59dbf630f407680bfdb
ThawingX <ThawingX@gmail.com>
2022-07-31 03:19:29 +0800

    sync

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <header mb-2>
     <nav flex justify-between bg-gray>
-      <img h-20 inline-flex src="logo.jpg" alt="">
+      <img h-20 inline-flex src="https://raw.githubusercontent.com/ThawingX/images/master/logo.jpg" alt="">
       <div m-4 inline-flex justify-center flex-wrap>
         <div>中医药装备标准与检验检测重点实验室</div>
         <div>Key Lab of Traditional Chinese Medicine Devices for Standards and Inspection</div>
@@ -55,7 +55,7 @@
       </el-form-item>
 
     </el-form>
-    <div >
+    <div>
       <el-button inline-block right-0 color-black type="success" @click="onSubmit()">submit</el-button>
     </div>
   </main>
