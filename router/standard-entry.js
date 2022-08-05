0e637364b7f8e3b16d9a0b78f4cb629ee409579d
thawingx <thawingx@gmail.com>
2022-08-05 16:17:41 +0800

    feat:new request

--- router/standard-entry.js
+++ router/standard-entry.js
@@ -5,12 +5,18 @@ const { Standard } = require('../schema/standard')
 const formidable = require("formidable")
 mongoose.connect('mongodb://119.3.243.150:3333/standard')
 
-router.post('/upload', (req, res) => {
+router.post('/upload', async (req, res) => {
   const body = req.body
-  Standard.create(body, (err) => {
-    if (err) res.send('error')
-    else res.send('success')
-  })
+  const data = await Standard.findOne({ 'key': body.key })
+  if (!data) {
+    Standard.create(body, (err) => {
+      if (err) res.send('cereate error')
+      else res.send('success')
+    })
+  }
+  else {
+    res.send('exists')
+  }
 })
 router.post('/uploadFile', (req, res) => {
   const form = new formidable.IncomingForm()
@@ -33,6 +39,25 @@ router.post('/uploadFile', (req, res) => {
     })
   )
   res.send('uploadFile')
+})
+
+router.post('/getList', async (req, res) => {
+  const data = await Standard.find({})
+  // console.log(data)
+  res.json({ data })
+})
+
+router.post('/checkStandard', async (req, res) => {
+  const body = req.body
+  const { search } = body
+  const data = await Standard.find({
+    $or: [
+      { key: search },
+      { chName: search }
+    ]
+  })
+  console.log(data)
+  res.json({ data })
 })
 
 router.get('/test', (req, res) => {
