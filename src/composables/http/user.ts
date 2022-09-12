d0e44c5f641c11b2dc77fa0c7ddf300c6cc64da8
thawingx <thawingx@gmail.com>
2022-09-12 19:26:51 +0800

    feat : table user

--- src/composables/http/user.ts
+++ src/composables/http/user.ts
@@ -15,3 +15,21 @@ export const $getAppliedList = async () => {
     headers: { },
   })
 }
+
+export const $allowApply = async (data = {}) => {
+  return axios({
+    method: 'post',
+    url: 'http://119.3.243.150:3399/user/review',
+    headers: { },
+    data: { ...data, doMethod: 'allow' },
+  })
+}
+
+export const $rejectApply = async (data = {}) => {
+  return axios({
+    method: 'post',
+    url: 'http://119.3.243.150:3399/user/review',
+    headers: { },
+    data: { ...data, doMethod: 'reject' },
+  })
+}
--- src/pages/menus/userManage.vue
+++ src/pages/menus/userManage.vue
@@ -1,9 +1,25 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useUserStore } from '~/stores/user'
+import { $allowApply, $rejectApply } from '~/composables/http/user'
 const userStore = useUserStore()
-const allowApply = function (row: any) {
-  console.log(row)
+const roleList = [
+  { label: '管理员', value: 'admin' },
+  { label: '科研人员', value: 'researcher' },
+  { label: '设备管理人员', value: 'equipManager' },
+  { label: '内部人员', value: 'staff' },
+]
+
+const tableRowClassName = function ({ row }) {
+  if (row.type === 'apply')
+    return 'warning-row'
+  else if (row.type === 'recall')
+    return 'success-row'
+  return ''
+}
+const allowApply = async function (row: any) {
+  const res = await $allowApply(row)
+  console.log(res)
 }
 
 const rejectApply = function (row: any) {
@@ -11,17 +27,29 @@ const rejectApply = function (row: any) {
 }
 onMounted(async () => {
   const res = await userStore.getAppliedList()
-  console.log(res)
 })
 </script>
 
 <template>
   <div class="container">
-    <el-table :data="userStore.appliedList" style="width: 100%">
+    <el-table :data="userStore.appliedList" style="width: 100%" :row-class-name="tableRowClassName">
+      <el-table-column type="index" label="序号" width="60" />
       <el-table-column prop="name" label="姓名" width="120" />
       <el-table-column prop="phone" label="手机" width="120" />
       <el-table-column prop="IDNumber" label="身份证" width="120" />
-      <el-table-column prop="email" label="邮箱" width="600" />
+      <el-table-column prop="email" label="邮箱" width="120" />
+      <el-table-column prop="role" label="角色" width="120">
+        <template #default="scope">
+          <el-select v-model="scope.row.role" placeholder="选择该用户的角色" size="small">
+            <el-option
+              v-for="item in roleList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+        </template>
+      </el-table-column>
       <el-table-column fixed="right" label="审核" width="120">
         <template #default="scope">
           <el-button link type="primary" size="small" @click="allowApply(scope.row)">
@@ -36,4 +64,11 @@ onMounted(async () => {
   </div>
 </template>
 
-<style scoped></style>
+<style>
+.el-table .warning-row {
+  --el-table-tr-bg-color: var(--el-color-warning-light-9);
+}
+.el-table .success-row {
+  --el-table-tr-bg-color: var(--el-color-success-light-9);
+}
+</style>
