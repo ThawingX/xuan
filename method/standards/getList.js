8305e3fdbbd24d204c6614228c8d276ff899824e
thawingx <thawingx@gmail.com>
2022-09-25 03:59:42 +0800

    feat: get standard list in limitNum

--- method/standards/getList.js
+++ method/standards/getList.js
@@ -1,10 +1,19 @@
 const { standardModel } = require('../../schema/standards')
 module.exports = async function (req, res, next) {
     try {
-        const data = await standardModel.find({}).limit(50)
-        return res.json({ data })
+        const {limitNum} = req.query
+        const data = await standardModel.find({}).limit(limitNum || 50)
+        return res.json({
+            code:1,
+            message:"获取成功",
+            result:data
+        })
     } catch (err) {
-        return res.send(err)
+        return res.json({
+            code: 0,
+            message:"获取失败",
+            result:err
+        })
     } finally {
     }
 
